25db5db5d205d22e49fad6504378cf1efb810c81
unknown <21908600@ncbnet.co.kr>
2020-11-16 16:52:38 +0900

    add drawing rectangle

--- click_and_crop.py
+++ click_and_crop.py
@@ -3,102 +3,67 @@ import cv2
 import numpy as np
 import os
  
-cropping = False
-cropping_up = False
-
-x_start, y_start, x_end, y_end = 0, 0, 0, 0
- 
-img_path = 'D:\\Dataset\\[Project]_HKC_H5_SAMPLE_TEST_IMAGE\\training_data_rescaling\\F8XPP_195_PS_Defect\\low\\none\\'
-img_list = os.listdir(img_path)
-img_list = [i for i in img_list if i[-1] == 'g']
-
-image = cv2.imread('D:\\temp_crop_test\\test.jpg')
-print("hello")
-print(type(image))
-oriImage = image.copy()
-
-roi_g = 1
- 
 def mouse_crop(event, x, y, flags, param):
     # grab references to the global variables
-    global x_start, y_start, x_end, y_end, cropping
- 
+    global refPoint, image, cropping
     # if the left mouse button was DOWN, start RECORDING
     # (x, y) coordinates and indicate that cropping is being
-    if event == cv2.EVENT_LBUTTONDOWN:
-        x_start, y_start, x_end, y_end = x, y, x, y
-        cropping = True
- 
+
+    
     # Mouse is Moving
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if cropping == True:
-            x_end, y_end = x, y
- 
+    if event == cv2.EVENT_MOUSEMOVE:
+        image = image_original.copy()
+        # define refPoint
+        tl_x = w-crop_size if x > w-crop_size//2 else max(0, x-crop_size//2) # to confine in image
+        tl_y = h-crop_size if y > h-crop_size//2 else max(0, y-crop_size//2) # to confine in image
+        br_x = tl_x + crop_size
+        br_y = tl_y + crop_size
+        refPoint = [(tl_x,tl_y),(br_x, br_y)]
+        # draw ractangle
+        cv2.rectangle(image, (refPoint[0][0], refPoint[0][1]), (refPoint[1][0], refPoint[1][1]), (255, 0, 0), 2)
+
     # if the left mouse button was released
     elif event == cv2.EVENT_LBUTTONUP:
-        # record the ending (x, y) coordinates
-        x_end, y_end = x, y
-        x_one, y_one = x, y
+        cropping = True
 
-        cropping = False # cropping is finished
- 
-        refPoint = [(x_start, y_start), (x_end, y_end)]
-        refPoint2 = [(x-149,y-149),(x+150,y+150)]
- 
-        if len(refPoint) == 2: #when two points were found
-            roi = oriImage[refPoint2[0][1]:refPoint2[1][1], refPoint2[0][0]:refPoint2[1][0]]
-            
-            cv2.imshow("Cropped", roi)
+    return
 
-            
-            
- 
+
+######################################################
+#####                PARAMETERS                  #####
+######################################################
+cropping = False
+print("Image Directory Path : ")
+img_path = input()
+print("Crop size :")
+crop_size = input()
+crop_size = int(crop_size)
+#img_path = 'D:\\Dataset\\WHTM_PH2_Mask_1\\review image\\PO03\\'
+img_list = os.listdir(img_path)
+img_list = [i for i in img_list if i.endswith('.jpg') or i.endswith('.bmp')]
+print(img_list)
+
 cv2.namedWindow("image", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("image", 800,800)
 cv2.setMouseCallback("image", mouse_crop)
- 
-#while True:
- 
-#    i = image.copy()
- 
-#    if not cropping:
-#        cv2.imshow("image", image)
- 
-#    elif cropping:
-#        cv2.rectangle(i, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
-#        cv2.imshow("image", i)
-
- 
-#    cv2.waitKey(1)
-
 
 for j in range(len(img_list)):
 
     image = cv2.imread(img_path + img_list[j])
-    
+    h, w, _ = np.shape(image)
+    image_original = image.copy()
     oriImage = image.copy()
-
     cropping = False
-    
-
+    cnt = 0
     while True:
- 
-        i = image.copy()
- 
+        #i = image.copy()
         if not cropping:
             cv2.imshow("image", image)
- 
         elif cropping:
-            cv2.rectangle(i, (x_start, y_start), (x_end, y_end), (255, 0, 0), 2)
-            cv2.imshow("image", i)
-
-            refPoint2 = [(x_end-149,y_end-149),(x_end+150,y_end+150)]
-            roi = oriImage[refPoint2[0][1]:refPoint2[1][1], refPoint2[0][0]:refPoint2[1][0]]
-
-            cv2.imwrite("..\\crop\\{}_crop.jpg".format(img_list[j][:-4]), roi)
-
+            cv2.imshow("image", image)
+            roi = oriImage[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
+            cv2.imwrite("..\\crop\\{}_crop.bmp".format(img_list[j][:-4]), roi)
             break
- 
         cv2.waitKey(1)
 
         
